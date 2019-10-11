1f5f31a9e4ebf4e3e7804f221af30cdbf87d6d07
3pacccccc <351489917@qq.com>
2019-10-12 00:05:05 +0800

    完成登陆接口 5-2完成

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,4 +1,42 @@
 package com.mmall.controller.portal;
 
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller  // spring的controller注解
+@RequestMapping("/user/")  // 相当于django下的app url前缀
 public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    /**
+     * 用户登陆
+     *
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody // 通过这个可以将返回值自动转换json格式，相当于django的return HttpJsonResponse作用
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        // service --> mybatis --> dao
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+        return response;
+
+
+    }
 }
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -1,4 +1,28 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+import javax.servlet.http.HttpSession;
+
 public interface IUserService {
+    ServerResponse<User> login(String userName, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse<String> selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String newPassword, String forgetToken);
+
+    ServerResponse<String> ResetPassword(String oldPassworod, String newPassword, User user);
+
+    ServerResponse<User> updateInformation(User user);
+
+    ServerResponse<User> getInformation(Integer id);
+
+    ServerResponse checkAdminRole(HttpSession session);
 }
